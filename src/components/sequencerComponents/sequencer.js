edd4e5cdee754bea8432ce052025e5a9bba5dbf5
Josh <greeny1248@gmail.com>
2023-02-21 11:46:59 +0000

    added bpm slider to sequencer

--- src/components/sequencerComponents/sequencer.js
+++ src/components/sequencerComponents/sequencer.js
@@ -33,7 +33,6 @@ const Sequencer = ({ player }) => {
         sequence[i][j] = { activated, triggered: j === time };
         if (triggered && activated) {
           player.get(lineMap[i]).start();
-
         }
       }
     }
@@ -41,22 +40,41 @@ const Sequencer = ({ player }) => {
   };
 
   useEffect(() => {
+    const beatDuration = (15 / bpm) * 1000;
     const timer = setTimeout(() => {
       if (playing) {
         setCurrentStep((currentStep + 1) % steps);
         nextStep(currentStep);
       }
-    }, 100 + Math.random() * 20);
+    }, beatDuration);
     return () => {
       clearTimeout(timer);
     };
   }, [currentStep, playing]);
 
+  const [bpm, setBpm] = useState(120);
+
+  const inputChange = (e) => {
+    console.log(e, "E");
+    setBpm(e.target.value);
+  };
   return (
     <>
-
+      <div className="bpm" style={{ padding: "20px" }}>
+        BPM: {bpm}
+        <div className="gain-control">
+          <input
+            name="gainValue"
+            type="range"
+            min="50"
+            max="240"
+            value={bpm}
+            onChange={inputChange}
+            step="1"
+          />
+        </div>
+      </div>
       <PlayButton playing={playing} onClick={() => setPlaying(!playing)} />
-
       <Grid sequence={sequence} toggleStep={toggleStep} />
     </>
   );
