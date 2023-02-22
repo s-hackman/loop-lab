5084af95a9fac568e11fb185d9ebf26893cad889
s-hackman <shisho.hackman@gmail.com>
2023-02-22 10:18:23 +0000

    adding user context and nav user display attempt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,25 @@ import sequencerSVG from "../svg/sequencer.svg";
 import homeSVG from "../svg/home.svg";
 import userSVG from "../svg/user.svg";
 import "../styles/navbar.css";
+import React, { useContext } from "react";
+import UserContext from "../context/userContext";
 
 const Navbar = () => {
+  const { loginUser } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+  console.log(loginUser);
+
   return (
     <nav>
       <h2 className="logo">Loop Lab</h2>
       <div className="linkContainer">
         <Link to="/login">
           <img src={userSVG} alt="login" />
-          <p id="username">Log in to save sounds...</p>
+          {!loginUser ? (
+            <p id="username">Log in to save sounds...</p>
+          ) : (
+            <p>{loginUser}</p>
+          )}
         </Link>
         <Link to="/">
           <img src={homeSVG} alt="go home" />
